a4361edb4f7a5385ae78997c655f0ef706a2ccf5
georgia drai <djonadre@gmail.com>
2022-04-10 01:02:59 +0200

    route structures crée

--- app/models/structure.rb
+++ app/models/structure.rb
@@ -1,3 +1,7 @@
 class Structure < ApplicationRecord
   belongs_to :user
+  CATEGORIES = %w(bar bistrot troquet restaurant brasserie foodtruck)
+  validates :name, :address, :cp, :district, presence: true
+  validates :category, inclusion: { in: CATEGORIES }
+  validates :user_id, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_09_220227) do
+ActiveRecord::Schema.define(version: 2022_04_09_223933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "structures", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "category"
+    t.integer "cp"
+    t.string "district"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_structures_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +39,5 @@ ActiveRecord::Schema.define(version: 2022_04_09_220227) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "structures", "users"
 end
